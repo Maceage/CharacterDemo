b351f2b121c137109573acc7123e11285024cc39
Graham Mace <graham@maceage.com>
2025-07-30 17:57:20 +1000

    Add grunt sound, damage animation notify, enemy blueprint

---
+++ Source/CharacterDemo/Private/DealDamageAnimNotifyState.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DealDamageAnimNotifyState.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+void UDealDamageAnimNotifyState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 4.5f, FColor::Yellow, TEXT("DealDamageAnimNotifyState: NotifyBegin"));
+}
+
+void UDealDamageAnimNotifyState::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	FVector StartTraceLocation = MeshComp->GetSocketLocation(TEXT("RightHandSocket"));
+	FVector EndTraceLocation = MeshComp->GetSocketLocation(TEXT("RightHandSocket"));
+	float Radius = 10;
+	bool bComplexTrace = false;
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(MeshComp->GetOwner());
+	bool bIgnoreSelf = true;
+	FLinearColor TraceColor = FLinearColor::Red;
+	FLinearColor TraceHitColor = FLinearColor::Green;
+	float DrawTime = 10;
+
+	TArray<FHitResult> HitArray;
+
+	const bool Hit = UKismetSystemLibrary::SphereTraceMulti(MeshComp, StartTraceLocation, EndTraceLocation, Radius,
+		UEngineTypes::ConvertToTraceType(ECC_Camera), bComplexTrace, ActorsToIgnore, EDrawDebugTrace::ForDuration,
+		HitArray, bIgnoreSelf, TraceColor, TraceHitColor, DrawTime);
+
+	if (Hit)
+	{
+		for (const FHitResult HitResult : HitArray)
+		{
+			UGameplayStatics::ApplyDamage(HitResult.GetActor(),
+				20,													// Damage
+				Cast<APawn>(MeshComp->GetOwner())->GetController(),	// Instigator
+				MeshComp->GetOwner(),								// Damage Causer (Actor)
+				UDamageType::StaticClass());						// Default damage type
+		}
+	}
+}
+
+void UDealDamageAnimNotifyState::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 4.5f, FColor::Yellow, TEXT("DealDamageAnimNotifyState: NotifyEnd"));
+}
